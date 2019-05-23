dd34ade6635f1bf625d50afc6799f05fa61d74af
s-tyd <shoyatoyoda@gmail.com>
2019-05-24 00:20:06 +0900

    add master src

---
+++ gulpfile.js
@@ -0,0 +1,23 @@
+var gulp = require('gulp');
+var watch = require('gulp-watch')
+var browserSync = require('browser-sync').create();
+
+
+// Static server
+gulp.task('browser-sync', function () {
+    browserSync.init({
+        server: {
+            baseDir: "./"
+        }
+    });
+});
+
+
+gulp.task('watch', function () {
+    // Callback mode, useful if any plugin in the pipeline depends on the `end`/`flush` event
+    watch('./**/*.html','./js/**/*.js', function () {
+        browserSync.reload();
+    });
+});
+
+gulp.task('default', ['browser-sync','watch'])
